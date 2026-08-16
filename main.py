81e6f180031d8b2a197ed9f70fe6be30c9f4a330
gizli9404-crypto <gizli9404@gmail.com>
2026-08-17 00:30:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, send_from_directory
 
-# Dosya yollarını garantiye almak için mutlak dizin tanımı
-basedir = os.path.abspath(os.path.dirname(__file__))
-public_dir = os.path.join(basedir, 'public')
+app = Flask(__name__)
 
-app = Flask(__name__, static_folder=public_dir, static_url_path='')
+# Kök dizin ve public klasörü
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PUBLIC_DIR = os.path.join(BASE_DIR, 'public')
 
 @app.route('/')
 def index():
-    return send_from_directory(public_dir, 'index.html')
+    return send_from_directory(PUBLIC_DIR, 'index.html')
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory(public_dir, path)
+    return send_from_directory(PUBLIC_DIR, path)
 
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
