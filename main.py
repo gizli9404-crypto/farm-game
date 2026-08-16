cba5df40db8f83dcde236c4adb1b300bfa3b898e
gizli9404-crypto <gizli9404@gmail.com>
2026-08-16 23:12:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import os
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='public', static_url_path='')
+# Dosya yollarını garantiye almak için mutlak dizin tanımı
+basedir = os.path.abspath(os.path.dirname(__file__))
+public_dir = os.path.join(basedir, 'public')
+
+app = Flask(__name__, static_folder=public_dir, static_url_path='')
 
 @app.route('/')
 def index():
-    return send_from_directory('public', 'index.html')
+    return send_from_directory(public_dir, 'index.html')
 
 @app.route('/<path:path>')
 def serve_static(path):
-    return send_from_directory('public', path)
+    return send_from_directory(public_dir, path)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
