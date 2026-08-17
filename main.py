42329a53668ca92ececba4aa61a7854114477ce6
gizli9404-crypto <gizli9404@gmail.com>
2026-08-17 09:22:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,11 +72,6 @@ if __name__ == '__main__':
     bot_thread = threading.Thread(target=run_bot)
     bot_thread.daemon = True
     bot_thread.start()
-
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-    import os
-
-if __name__ == "__main__":
+    
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
